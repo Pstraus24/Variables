193c8d52cdd43789b27bc3069c6bb3d17f140222
Peter E. Straus <pstraus@students.mchenry.edu>
2018-01-27 22:44:18 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // initialize variables
+        byte b = 12;
+        int i = 124;
+        short sh = 57;
+        long l = 1942L;
+        float fl = 17.34f;
+        double dbl = 19.3d;
+        boolean boo = false;
+        char m = 'm';
+
+        // display variables
+        System.out.println("byte demo: " + b);
+        System.out.println("short demo: " + sh);
+        System.out.println("integer demo: " + i);
+        System.out.println("long demo: " + l);
+        System.out.println("float demo: " + fl);
+        System.out.println("double demo: " + dbl);
+        System.out.println("boolean demo: " + boo);
+        System.out.println("char demo: " + m);
+    }
+}
